1cd7cb00ef879c7ba69df8aa2d894a8732cd2b63
Mahamadoucode01 <issakaoumaroumahamadousabiou@gmail.com>
2023-02-11 11:26:59 +0100

    message

---
+++ Js01/oneToTen.js
@@ -0,0 +1,9 @@
+// Déclaration de la variable pour stocker la somme
+let sum = 0;
+// Boucle pour itérer à travers les nombres de 1 à 10
+for (let i = 1; i <= 10; i++) {
+    // Ajouter chaque nombre à la somme
+    sum += i;
+}
+// Afficher la somme finale
+console.log("sum of numbers 1 to 10 : " + sum);
---
+++ Js01/weekendsVerified.js
@@ -0,0 +1,6 @@
+let day = prompt("Entrer le jour de la semaine :");
+if (day === "Samedi" || day === "dimanche") {
+    console.log("weekend");
+} else {
+    console.log("weekday");
+}
