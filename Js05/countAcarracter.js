6bb0ea4edae8b5e865631649a90722f09a872233
Mahamadoucode01 <issakaoumaroumahamadousabiou@gmail.com>
2023-02-19 23:14:29 +0100

    message05

---
+++ Js05/countAcarracter.js
@@ -0,0 +1,15 @@
+let sentence = "Bonjour, chers candidats comment vous allez aujourd'hui";
+
+function countAcaracter(sentence) {
+  let count = 0;
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i].toLowerCase() === 'a') {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log("le nombre d'occurrence de 'a' est: " +countAcaracter(sentence));
+
+module.exports=countAcaracter;
